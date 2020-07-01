305c07c54ec4558190e909018d1e873821e39a39
Axel Segundo <axeljasegundo@gmail.com>
2020-07-01 13:46:19 -0500

    Cambios v4

--- JS/efectos.js
+++ JS/efectos.js
@@ -1,7 +1,7 @@
 $(document).ready (function(){
     var acercaDe = $('#acerca-de').offset().top,
-        Trabajos = $('#trabajos').offset().top,
-        Contacto = $('#contacto').offset().top;
+        trabajos = $('#trabajos').offset().top,
+        contacto = $('#contacto').offset().top;
 
     $('#btn-acerca-de').on('click', function(e){
         e.preventDefault();
@@ -12,13 +12,13 @@ $(document).ready (function(){
     $('#btn-trabajos').on('click', function(e){
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: Trabajos
+            scrollTop: trabajos
         }, 500);
     });
     $('#btn-contacto').on('click', function(e){
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: Contacto
+            scrollTop: contacto
         }, 500);
     });
    
